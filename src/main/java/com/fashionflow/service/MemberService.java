b08df9f1dec33e80ba1fd7f4068f44bfc65edca7
Kwon <kwonabraham@naver.com>
2024-04-27 03:35:52 +0900

    Bcrypt 추가

--- src/main/java/com/fashionflow/service/MemberService.java
+++ src/main/java/com/fashionflow/service/MemberService.java
@@ -6,6 +6,11 @@ import com.fashionflow.entity.Member;
 import com.fashionflow.repository.MemberRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -13,18 +18,21 @@ import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+@Transactional
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
     //회원 등록 메소드
-    @Transactional
-    public void registerMember(MemberFormDTO memberFormDTO) {
+    public void registerMember(MemberFormDTO memberFormDTO, PasswordEncoder passwordEncoder) {
+
+        //Bcrypt 인코드
+        String encodedPassword = passwordEncoder.encode(memberFormDTO.getPwd());
 
         Member member = Member.builder()
                 .name(memberFormDTO.getName())
                 .email(memberFormDTO.getEmail())
-                .pwd(memberFormDTO.getPwd())
+                .pwd(encodedPassword)
                 .nickname(memberFormDTO.getNickname())
                 .phone(memberFormDTO.getPhone())
                 .birth(memberFormDTO.getBirth())
@@ -35,12 +43,14 @@ public class MemberService {
                 .regdate(LocalDateTime.now())
                 .build();
 
+        //중복 확인
         checkDuplicate(member);
 
         memberRepository.save(member);
 
     }
 
+    //중복 체크 메소드
     private void checkDuplicate(Member member) {
         Member checkMember = memberRepository.findByEmailOrPhoneOrNickname(member.getEmail(), member.getPhone(), member.getNickname());
         if (checkMember != null) {
@@ -54,5 +64,20 @@ public class MemberService {
                 throw new IllegalStateException("중복되는 닉네임입니다.");
             }
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPwd())
+                .roles(member.getRole().toString())
+                .build();
     }
 }

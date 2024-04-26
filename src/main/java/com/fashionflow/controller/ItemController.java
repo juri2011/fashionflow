db54e56ac5d457765bf45ff450d28e369066d476
오주리 <ojl98@naver.com>
2024-04-26 12:07:23 +0900

    view에서 카테고리 출력 확인

--- src/main/java/com/fashionflow/controller/ItemController.java
+++ src/main/java/com/fashionflow/controller/ItemController.java
@@ -1,7 +1,9 @@
 package com.fashionflow.controller;
 
+import com.fashionflow.entity.Category;
 import com.fashionflow.entity.Item;
 import com.fashionflow.entity.ItemTag;
+import com.fashionflow.repository.CategoryRepository;
 import com.fashionflow.repository.ItemRepository;
 import com.fashionflow.repository.ItemTagRepository;
 import com.fashionflow.repository.MemberRepository;
@@ -24,6 +26,8 @@ public class ItemController {
 
     private final ItemTagRepository itemTagRepository;
 
+    private final CategoryRepository categoryRepository;
+
     //상품 리스트 출력
     @GetMapping("/item/{itemId}")
     public String itemDetali(Model model, @PathVariable("itemId") Long itemId){
@@ -34,8 +38,26 @@ public class ItemController {
 
         List<ItemTag> itemTagList = itemTagRepository.findByItemId(item.getId()); //조회한 상품의 아이디로 상품 태그 찾기
 
+        /* item 객체 안에 저장된 카테고리 id를 이용해서 해당 카테고리 객체를 받아옴 */
+        Category category = categoryRepository.findById(item.getCategory().getId())
+                .orElseThrow(() ->
+                new EntityNotFoundException("카테고리가 존재하지 않습니다. id = " + item.getCategory().getId()));
+
+        Category parentCategory = null;
+        if(category.getParent() != null){
+            parentCategory = categoryRepository.findById(category.getParent().getId())
+                    .orElseThrow(() ->
+                    new EntityNotFoundException("카테고리가 존재하지 않습니다. id = " + item.getCategory().getId()));
+        }
+
+        System.out.println("=============================== 하위카테고리 : "+category);
+        System.out.println("=============================== 상위카테고리 : "+parentCategory);
+
+        //얘네 FormDto로 묶어서 전달하는게 좋을듯
         model.addAttribute("item", item); //상품 전달
         model.addAttribute("itemTagList", itemTagList); //상품 태그 전달
+        model.addAttribute("category", category); //하위 카테고리
+        model.addAttribute("parentCategory", parentCategory); //상위 카테고리
 
         return "item/itemDetail";
     }

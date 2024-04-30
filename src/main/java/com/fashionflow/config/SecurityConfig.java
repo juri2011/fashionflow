c4bc81957ef98988c75b5fc638f29b4ea1a21283
nowod9712 <154123670+nowod9712@users.noreply.github.com>
2024-04-30 17:27:33 +0900

    Security Config 주석 정리 / 로그인에 비밀번호 찾기 버튼 추가 / 메인 로그아웃 버튼 정렬

--- src/main/java/com/fashionflow/config/SecurityConfig.java
+++ src/main/java/com/fashionflow/config/SecurityConfig.java
@@ -47,28 +47,5 @@ public class SecurityConfig{
         return new BCryptPasswordEncoder();
     }
 
-/*
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(auth -> auth
-                        .requestMatchers("/**").permitAll() // 모든 경로에 대해 인증 없이 접근 허용
-                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
-                )
-                .formLogin(form -> form
-                        .loginPage("/members/login") // 로그인 페이지 경로 설정
-                        .defaultSuccessUrl("/", true) // 로그인 성공 후 리다이렉트할 기본 경로
-                        .failureUrl("/members/login/error") // 로그인 실패 시 리다이렉트할 경로
-                        .usernameParameter("email") // username 파라미터를 email로 사용
-                        .passwordParameter("password") // password 파라미터 사용
-                        .permitAll() // 모든 사용자가 로그인 페이지 접근 허용
-                );
 
-        return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }*/
 }

458390d77d7ae7510214390914d8b3e366a336ee
오주리 <ojl98@naver.com>
2024-04-30 15:22:50 +0900

    ReportItemDTO 생성

---
+++ src/main/java/com/fashionflow/dto/ReportItemDTO.java
@@ -0,0 +1,15 @@
+package com.fashionflow.dto;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReportItemDTO {
+    
+}

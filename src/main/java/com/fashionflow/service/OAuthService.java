24a28eb42279e7df09ce8256c6234c496cacd5bb
kty <kwonabraham@naver.com>
2024-05-03 12:23:57 +0900

    난수 문자열 생성 메서드 추가

--- src/main/java/com/fashionflow/service/OAuthService.java
+++ src/main/java/com/fashionflow/service/OAuthService.java
@@ -6,8 +6,12 @@ import com.fashionflow.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +19,7 @@ public class OAuthService {
 
     private final MemberRepository memberRepository;
     private final MemberService memberService;
+
     //회원 등록 메소드
     public void registerOAuth(MemberFormDTO memberFormDTO, PasswordEncoder passwordEncoder) {
 
@@ -45,4 +50,21 @@ public class OAuthService {
         memberRepository.save(member);
 
     }
+
+    //난수 문자열 생성
+    public static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+        Random rand = new Random();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = rand.nextInt(characters.length());
+            char randomChar = characters.charAt(randomIndex);
+            stringBuilder.append(randomChar);
+        }
+        return stringBuilder.toString();
+    }
+
+
+
+
 }

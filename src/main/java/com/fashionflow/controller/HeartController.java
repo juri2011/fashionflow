065424baec802ea6fd931afa62897d86fad82a8d
오주리 <ojl98@naver.com>
2024-05-07 17:14:10 +0900

    상품 찜 작업중

--- src/main/java/com/fashionflow/controller/HeartController.java
+++ src/main/java/com/fashionflow/controller/HeartController.java
@@ -11,16 +11,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class HeartController {
     private final HeartService heartService;
 
+    
+    //작업중
     @PostMapping("/heart/addHeart")
-    public @ResponseBody ResponseEntity addHeart(@RequestBody Long id, Principal principal){
+    public @ResponseBody ResponseEntity addHeart(@RequestBody Map<String, Long> requestData, Principal principal){
+        Long id = requestData.get("id");
         System.out.println("상품번호 : " + id);
-        System.out.println("사용자 이메일 : " + principal.getName());
+        /*
+        try{
+            System.out.println("사용자 이메일 : " + principal.getName());
+        }catch(Exception e){
+            return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+        */
         //사용자가 이미 찜한 상품인지 확인
 
         return new ResponseEntity<Long>(1L, HttpStatus.OK);

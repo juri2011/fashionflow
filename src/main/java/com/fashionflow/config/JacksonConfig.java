affb1764ad19d51d83ee20301e52f95aef2297f1
kty <kwonabraham@naver.com>
2024-05-10 14:51:06 +0900

    비동기 무한 스크롤 구현

--- src/main/java/com/fashionflow/config/JacksonConfig.java
+++ src/main/java/com/fashionflow/config/JacksonConfig.java
@@ -1,6 +1,8 @@
 package com.fashionflow.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +11,10 @@ public class JacksonConfig {
 
     @Bean // JSON 직렬화 & 역직렬화
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+
     }
+
 }
--- src/main/java/com/fashionflow/controller/ListController.java
+++ src/main/java/com/fashionflow/controller/ListController.java
@@ -1,10 +1,25 @@
 package com.fashionflow.controller;
 
+import com.fashionflow.entity.Item;
+import com.fashionflow.entity.ItemTag;
 import com.fashionflow.repository.ItemRepository;
+import com.fashionflow.repository.ItemTagRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,11 +27,22 @@ public class ListController {
 
     private final ItemRepository itemRepository;
 
+
     @GetMapping("/list")
     public String listPage(Model model){
 
-        model.addAttribute("productLists",itemRepository.findAll());
+        Pageable firstPageWithEightItems = PageRequest.of(0, 8); // 첫 페이지, 항목 8개
+        model.addAttribute("productLists", itemRepository.findAll(firstPageWithEightItems).getContent());
 
         return "/list";
+    }
+
+    @GetMapping("/list/more")
+    @ResponseBody
+    public ResponseEntity<List<Item>> listMoreProducts(@RequestParam("page") int currentPage){
+        Pageable pageable = PageRequest.of(currentPage, 1); // 페이지 번호와 페이지당 항목 수
+        List<Item> items = itemRepository.findAll(pageable).getContent();
+
+        return ResponseEntity.ok(items); // ResponseEntity를 통해 상품 목록을 JSON 형식으로 반환
     }
 }

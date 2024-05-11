a7db7b7b64b5e3025355e73720e9a9e4d05aeec5
오주리 <ojl98@naver.com>
2024-05-11 23:27:14 +0900

    채팅 날짜 시간 출력

--- src/main/java/com/fashionflow/dto/chat/ChatMessageDTO.java
+++ src/main/java/com/fashionflow/dto/chat/ChatMessageDTO.java
@@ -1,9 +1,16 @@
 package com.fashionflow.dto.chat;
 
 import com.fashionflow.entity.ChatMessage;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
@@ -15,13 +22,18 @@ public class ChatMessageDTO {
     private String roomId;
     private String writer;
     private String message;
-    //private LocalDateTime msgDate;
+
+    @JsonIgnore
+    private LocalDateTime msgDate;
+    private String msgDateFormat;
 
     public static ChatMessageDTO entityToDTO(ChatMessage chatMessage){
         return ChatMessageDTO.builder()
                 .roomId(chatMessage.getChatRoom().getUuid())
                 .writer(chatMessage.getSender())
                 .message(chatMessage.getMessage())
+                .msgDate(chatMessage.getSendDate())
+                .msgDateFormat(chatMessage.getSendDate().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")))
                 .build();
     }
 }

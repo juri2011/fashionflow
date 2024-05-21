9efde46672d0b4de6ddd31fd94fed8c87a1c96b1
kty <kwonabraham@naver.com>
2024-05-21 16:21:29 +0900

    constant 주석 추가

--- src/main/java/com/fashionflow/constant/ReportManageCommand.java
+++ src/main/java/com/fashionflow/constant/ReportManageCommand.java
@@ -3,12 +3,15 @@ package com.fashionflow.constant;
 public enum ReportManageCommand {
     SUSPEND("중지"), DELETE("삭제"), ETC("직접처리");
 
+    // 각 열거형 상수에 대한 설명
     private final String description;
 
+    // 생성자
     ReportManageCommand(String description) {
         this.description = description;
     }
 
+    // 설명을 반환하는 getter 메서드
     public String getDescription() {
         return description;
     }
--- src/main/java/com/fashionflow/constant/ReportStatus.java
+++ src/main/java/com/fashionflow/constant/ReportStatus.java
@@ -3,12 +3,15 @@ package com.fashionflow.constant;
 public enum ReportStatus {
     WAITING("처리대기중"), COMPLETE("처리완료");
 
+    // 각 열거형 상수에 대한 설명
     private final String description;
 
+    // 생성자
     ReportStatus(String description) {
         this.description = description;
     }
 
+    // 설명을 반환하는 getter 메서드
     public String getDescription() {
         return description;
     }
